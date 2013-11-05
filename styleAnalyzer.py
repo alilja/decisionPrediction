09f49000706c9086a7d87a8ac6455f10945a7df6
Andrew Lilja (Work) <alilja@iastate.edu>
2013-11-05 13:36:27 -0600

    recreated decisiontracer function

--- styleAnalyzer.py
+++ styleAnalyzer.py
@@ -72,7 +72,7 @@ class DecisionTracer:
             optionTimes[entry["option"]] += entry["timeViewed"]
         return optionTimes
 
-    def searchIndex(self):
+    def caclulateSearchIndex(self):
         intra, inter, mixed = countTransitions()
         searchIndex = (inter - intra)/(inter + intra)
         debugLog(searchIndex)
@@ -123,7 +123,7 @@ class DecisionTracer:
             ARList.append(avg)
             NBoxList.append(nbox)
 
-        return pearsonr(ARList, NBoxList)
+        return self.pearsonr(ARList, NBoxList)
 
     def method4(self):
         ###################################
@@ -204,168 +204,20 @@ class DecisionTracer:
 
         return bestMatchName
 
-#list-of-dict, list-of-str, list-of-str --> str
-def analyzeDecisionStyle(matrix, rankOrder, weightedAttributes, minCorrelationPercentage=0.7, timeTolerance=0.8):
-    decisionsMade = matrix.getDecisionList()
-    attributes = matrix.getAttributes()
-    options = matrix.getOptions()
-
-    rankedDecisions = []
-    for item in rankOrder:
-        decisionEntry = matrix.findDecision(item)
-        rankedDecisions.append(decisionEntry["option"])
-
-    optionTimes = {}
-
-    numOPWISE = 0
-    numATTWISE = 0
-    numMIXED = 0
-
-    # Figure out what the ratio of option-wise, attribute-wise, and mixed transitions there are
-    iterDecisionList = iter(decisionsMade)
-    iterDecisionList.__next__()
-    previousDecision = decisionsMade[0]
-    for entry in iterDecisionList:
-        if(previousDecision["attribute"] == entry["attribute"]): #within attribute switch; option changes #intradimensional
-            numATTWISE += 1
-        elif(previousDecision["option"] == entry["option"]):     #within option switch; attribute changes #interdimensional
-            numOPWISE += 1
-        else:                                                    #both the option and attribute change
-            numMIXED += 1
-        if(entry["option"] not in optionTimes): # sum the total amount of time
-                                                  # spent looking at each option
-            optionTimes[entry["option"]] = 0
-        optionTimes[entry["option"]] += entry["timeViewed"]
-
-        previousDecision = entry
-
-    searchIndex = (numOPWISE - numATTWISE)/(numOPWISE + numATTWISE)
-    debugLog(searchIndex)
-
-    if(searchIndex > 0):
-
-        ################################################
-        ### CAPTURING EQW, LIM, LVA, MAU, DIS, & SAT ###
-        ################################################
-
-        OPATTRatio = ((len(attributes) - 1)*len(options))/(len(options) - 1) #ratio of options to attributes
-        if((numOPWISE/(numATTWISE+numMIXED))/OPATTRatio >= minCorrelationPercentage): #higher values mean more precision, but more potential false negatives
-            
-            ###################################
-            # SEPARATE EQW, MAU, LIM, AND LVA #
-            ###################################
-            # EQW ranking of options is just the sum of all the option's utility values.
-            # MAU ranking is sum of all utility values, with weights according to how good each attribute is (weights must add up to 1)
-            # LIM chooses the option with the worst value of the least important attribute
-            # LVA chooses the option with the least variance across attribute values
-
-            # TODO(alilja@iastate.edu) for some reason EQW/LIM and MAU/LVA are getting called at the same time. It's probably because of the matrix settings.
-
-            ranks = {"MAU":[],"LIM":[],"LVA":[], "EQW":[]}
-
-            lowestAttribute = min(weightedAttributes, key=weightedAttributes.get)
-
-            for option in options: #iterate through each option and grab a list of all the decisions connected to it
-                    optionDecisions = [d for d in matrix.getDecisions() if d["option"] == option] #grab all the decisions that belong to that option
-
-                    utilityValues = [d["utility"] for d in optionDecisions]
-                    utilityAverage = sum(utilityValues)/len(options)
-
-                    #EQW ranks
-                    ranks["EQW"].append((sum(utilityValues), option))
-
-                    MAUUtility = 0
-                    variance = 0
-
-                    for decision in optionDecisions: #flip through each decision in the ones we grabbed
-                        #MAU ranks
-                        MAUUtility += decision["utility"] * weightedAttributes[decision["attribute"]] #weight that decision's utility by the attribute weight
-
-                        #LIM ranks
-                        if(decision["attribute"] == lowestAttribute):
-                            ranks["LIM"].append((decision["utility"], option))
-
-                        #LVA ranks
-                        variance += (decision["utility"] - utilityAverage) ** 2
-
-                    variance = variance / (len(options) - 1)
-                    ranks["LVA"].append((variance, option))
-
-                    ranks["MAU"].append((MAUUtility, option))
-
-            ranks["EQW"].sort(reverse=True)
-            ranks["MAU"].sort(reverse=True)
-            ranks["LIM"].sort()
-            ranks["LVA"].sort()
-
-            bestMatchName = ""
-            bestMatchScore = 1000
-
-            for style, currentList in ranks.items():            
-                deviation = 0
-
-                print(style, currentList)
-                currentUtilities, currentOptions = zip(*currentList)
-
-                for predictedItemRank, predictedItem in enumerate(currentOptions):
-                    #grab the rank of that option in the user-inputed rank list
-                    empiricalRank = rankedDecisions.index(predictedItem) 
-                    deviation = (predictedItemRank - empiricalRank) ** 2    
-
-                if(deviation < bestMatchScore):
-                    bestMatchScore = deviation
-                    bestMatchName = style
-
-            return bestMatchName
+    def DecisionTracer(self, correlation = 0.7):
+        searchIndex = self.caclulateSearchIndex()
+        if(searchIndex > 0):
+            op, att, mix = self.countTransitions()
+            if(self.method1(op, att, mix) >= correlation):
+                return self.method4()
+            else:
+                return "DIS|SAT"
         else:
-            return "DIS|SAT"
-
-    elif(searchIndex < 0):
-
-        #####################################################
-        ### CAPTURING ADD, DOM, MAJ, MCD, EBA, LEX, & REC ###
-        #####################################################
-
-        #grab the attributes for every decision the user made
-        attributesViewedOrdered = [x["attribute"] for x in decisionsMade]
-
-        #create an empty list of lists as long as there are attributes 
-        attributeRanks = [[] for i in range(0, len(attributes))] 
-
-        # run through and increase the rank equal to the order each decision 
-        # was viewed; earlier decisions are ranked more highly
-        for i in range(0, len(attributesViewedOrdered)):
-            rank = i+1
-            attributeViewed = attributesViewedOrdered[i] # the attribute 
-                                                         # that was viewed
-            attributeNumber = attributes.index(attributeViewed) 
-            attributeRanks[attributeNumber].append(rank) 
-
-        ARList = []
-        NBoxList = []
-
-        for attributeRankList in attributeRanks:
-            nbox = len(attributeRankList)
-            gross = sum(attributeRankList)
-            avg = gross/nbox
-
-            ARList.append(avg)
-            NBoxList.append(nbox)
-
-        correlation = pearsonr(ARList, NBoxList)
-        
-        if(correlation < 0):
-            return("EBA|LEX|REC")
-        else:
-            minTime = min(optionTimes.values())
-            maxTime = max(optionTimes.values())
-            debugLog(minTime, maxTime)
-            if(minTime/maxTime >= 0.8):
-                return("DOM|MAJ")
-            return("ADD|MCD")
-    else:
-        return "ERR: Search index = 0.0"
-
+            if(self.method3() < 0):
+                return("EBA|LEX|REC")
+            else:
+                return self.method2()
+                
 decisions = DecisionMatrix()
 data = ""
 rankedDecisions = []
